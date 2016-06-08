f5da470843a3e061db9f19f33a1960c9ec3302f7
Marek Raida <marek.raida@lmc.eu>
2016-06-08 23:04:29 +0200

    SOme minor bulk tweaks

--- es-feed-bulk2.php
+++ es-feed-bulk2.php
@@ -1,24 +1,42 @@
 <?php
-ini_set('memory_limit', '2048M');
+ini_set('memory_limit', '512M');
 require ('/Users/xrevelon/git/ulozto-web/Nodus/Security/IntEncrypt.php');
 require ('/Users/xrevelon/cnf.php');
 
-echo "\n\n\n";
+echo "\n\n";
 
 $mysqli = new mysqli($cnf['h'], $cnf['u'], $cnf['p'], $cnf['d']);
 
-/* check connection */
 if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
 
-$lastFileId = 999999999;
-$success = 0; $fail = 0;
+$buffer = 0;
+$success = 0;
+$fail = 0;
+$command = "";
 
-while ($lastFileId) {
-    if ($result = $mysqli->query("
+function feedData($command) {
+    $temp = tempnam('.', 'feeder_');
+    file_put_contents($temp, $command);
+    $rawResponse = shell_exec('curl -XPOST "http://localhost:9200/_bulk" --data-binary "@'.$temp.'"');
+    //$rawResponse = shell_exec('curl -XPOST "http://es1.farm.int.nds:9200/_bulk" --data-binary "@'.$temp.'"');
+    $response = json_decode($rawResponse);
+    if ($response->errors === false) {
+        echo "\nSuccess ";
+        // tidy up the mess in current directory
+        unlink($temp);
+        return true;
+    } else {
+        echo "\nFail on {$temp} file with: " . $rawResponse;
+        // do not unlink file to keep it for potential analysis
+        return false;
+    }
+}
 
+echo "\nExecuting SQL query...";
+if ($result = $mysqli->query("
     select f.id, name, keywords_name as keywords, f.hashid, f.upload_date as uploaded, uploader_geoipcountry as geoipcountry, 
     contentType, fh.size as sizeInKB, if(displayStatus in ('safe', 'maybe_safe'),'UT','PF') as realm, rating_status as rating, 
     if(fhm.width>500 and fhm.height>500,'HQ','LQ') as quality, length as lengthInSec, 
@@ -28,50 +46,44 @@ while ($lastFileId) {
     left join file_hashflags fh using (hashid)
     left join file_hash_multimedia fhm using (hashid)
     left join file_upload_data fud on (f.id=fud.file_id)
-    where id < {$lastFileId} and status='ok' and public='public' and banned=0 
+    where status='ok' and public='public' and banned=0 
     and virusFound<2 and displayStatus not in ('illegal','maybe_illegal')
-    order by id desc limit 10000;
+")) {
 
-    							 ")) {
-        $resultCount = mysqli_num_rows($result);
-    	echo "Number of files: " . $resultCount . "\n\n";
+    $resultCount = mysqli_num_rows($result);
+	echo "Query done, Number of files to index: " . $resultCount . "\n\n";
 
-        if (!$resultCount) {
-            echo ("\nNo more data to fetch, ending....");
-            $lastFileId = 0;
-            break;
+    while ($row = $result->fetch_assoc()) {
+    	$command .= '{ "index" : { "_index" : "files3", "_type" : "public", "_id" : "' . $row['id'] . '" } }' . "\n";
+        unset($row['id']);
+        $command .= json_encode($row, JSON_HEX_APOS | JSON_UNESCAPED_UNICODE) . "\n";
+        $buffer++;
+
+        if (($buffer % 6666) === 0) {
+            $ret = feedData($command);
+            if ($ret) {
+                $success++;
+            } else {
+                $fail++;
+            }
+            $buffer = 0;
+            $command = "";
         }
-
-        $command = "";
-        while ($row = $result->fetch_assoc()) {
-        	$command .= '{ "index" : { "_index" : "files2", "_type" : "public", "_id" : "' . $row['id'] . '" } }' . "\n";
-            $lastFileId = $row['id'];
-            unset($row['id']);
-            $command .= json_encode($row, JSON_HEX_APOS | JSON_UNESCAPED_UNICODE) . "\n";
-        }
-
-        $temp = tempnam('.', 'feed');
-        file_put_contents($temp, $command);
-        //$rawResponse = shell_exec('curl -XPOST "http://localhost:9200/_bulk" --data-binary "@'.$temp.'"');
-        $rawResponse = shell_exec('curl -XPOST "http://es1.farm.int.nds:9200/_bulk" --data-binary "@'.$temp.'"');
-        $response = json_decode($rawResponse);
-        if ($response->errors === false) {
-            echo "\nSuccess ";
-            $success++;
-            // tidy up the mess in current directory
-            unlink($temp);
-        } else {
-            echo "\nFail on {$temp} file with: " . $rawResponse;
-            $fail++;
-            // do not unlink file to keep it for potential analysis
-        }
-
-        /* free result set */
-        $result->close();
     }
+
+    if ($command) {
+        $ret = feedData($command); // send the rest
+        if ($ret) {
+            $success++;
+        } else {
+            $fail++;
+        }
+    }
+
+    $result->close();
+} else {
+    echo "\nDB query failed!";
 }
-
-
 
 
 $mysqli->close();
@@ -83,11 +95,11 @@ exit();
 
 ?>
 
-DELETE /files2
+DELETE /files3
 
-GET /files2/_stats
+GET /files3/_stats
 
-PUT /files2
+PUT /files3
 {
   "mappings": {
     "public": {
@@ -139,24 +151,4 @@ PUT /files2
       }
     }
   }
-}
-
-
-POST /files/_search
-{
-    "query": {
-        "query_string": {
-            "query": "petr rar",
-            "fields": ["keywords"]
-        }
-    },
-    "filter": {
-        "term": {
-            "passwordProtected": false 
-        }
-    },
-    "from" : 0, "size" : 10,
-    "sort" : [
-        {"uploaded" : "desc"}
-   ]
 }
