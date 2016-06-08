d8ff08d81392a0beccb620bde23750e36fe79097
Marek Raida <marek.raida@lmc.eu>
2016-06-08 16:23:01 +0200

    Some improvements in ES, more searches and bulk index

---
+++ es-feed-bulk.php
@@ -0,0 +1,160 @@
+<?php
+ini_set('memory_limit', '2048M');
+require ('/Users/xrevelon/git/ulozto-web/Nodus/Security/IntEncrypt.php');
+require ('/Users/xrevelon/cnf.php');
+
+echo "\n\n\n";
+
+$mysqli = new mysqli($cnf['h'], $cnf['u'], $cnf['p'], $cnf['d']);
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$lastFileId = 999999999;
+$success = 0; $fail = 0;
+
+while ($lastFileId) {
+    if ($result = $mysqli->query("
+
+    select f.id, name, keywords_name as keywords, f.hashid, f.upload_date as uploaded, uploader_geoipcountry as geoipcountry, 
+    contentType, fh.size as sizeInKB, if(displayStatus in ('safe', 'maybe_safe'),'UT','PF') as realm, rating_status as rating, 
+    if(fhm.width>500 and fhm.height>500,'HQ','LQ') as quality, length as lengthInSec, 
+    if(streamable,1,0) as streamable, if(thumbVideo='',false,true) as liveFreeStreaming, 
+    if(archiveProtected>0 and password is not null,true,false) as archiveProtected, if(password,true,false) as passwordProtected 
+    from file f
+    left join file_hashflags fh using (hashid)
+    left join file_hash_multimedia fhm using (hashid)
+    left join file_upload_data fud on (f.id=fud.file_id)
+    where id < {$lastFileId} and status='ok' and public='public' and banned=0 
+    and virusFound<2 and displayStatus not in ('illegal','maybe_illegal')
+    limit 5000;
+
+    							 ")) {
+        $resultCount = mysqli_num_rows($result);
+    	echo "Number of files: " . $resultCount . "\n\n";
+
+        if (!$resultCount) {
+            echo ("\nNo more data to fetch, ending....");
+            $lastFileId = 0;
+            break;
+        }
+
+        $command = "";
+        while ($row = $result->fetch_assoc()) {
+        	$command .= '{ "index" : { "_index" : "files2", "_type" : "public", "_id" : "' . $row['id'] . '" } }' . "\n";
+            $lastFileId = $row['id'];
+            unset($row['id']);
+            $command .= json_encode($row, JSON_HEX_APOS | JSON_UNESCAPED_UNICODE) . "\n";
+        }
+
+        $temp = tempnam('.', 'feed');
+        file_put_contents($temp, $command);
+        $rawResponse = shell_exec('curl -XPOST "http://localhost:9200/_bulk" --data-binary "@'.$temp.'"');
+        $response = json_decode($rawResponse);
+        if ($response->errors === false) {
+            echo "\nSuccess";
+            $success++;
+        } else {
+            echo "\nFail with: " . $rawResponse;
+            $fail++;
+        }
+        // tidy up the mess in current directory
+        //unlink($temp);
+
+        /* free result set */
+        $result->close();
+    }
+}
+
+
+
+
+$mysqli->close();
+
+echo "\n\nFinished with OKs: {$success}   and   KOs: {$fail}  of batches... \n";
+
+
+exit();
+
+?>
+
+DELETE /files2
+
+GET /files2/_stats
+
+PUT /files2
+{
+  "mappings": {
+    "public": {
+      "properties": {
+        "name": {
+            "type": "string",
+            "index": "no"
+        },
+        "keywords": {
+            "type": "string"
+        },
+        "hashid": {
+            "type": "long"
+        },
+        "uploaded": {
+          "type":   "date",
+          "format": "yyy-MM-dd HH:mm:ss"
+        },
+        "geoipcountry": {
+            "type": "string"
+        },
+        "contentType": {
+            "type": "string"
+        },
+        "sizeInKB": {
+            "type": "long"
+        },
+        "realm": {
+            "type": "string"
+        },
+        "rating": {
+            "type": "short"
+        },
+        "quality":   {
+            "type": "string"
+        },
+        "lengthInSec": {
+            "type": "integer"
+        },
+        "streamable": {
+            "type": "boolean"
+        },
+        "liveFreeStreaming": {
+            "type": "boolean"
+        },
+        "passwordProtected": {
+            "type": "boolean"
+        }
+      }
+    }
+  }
+}
+
+
+POST /files/_search
+{
+    "query": {
+        "query_string": {
+            "query": "petr rar",
+            "fields": ["keywords"]
+        }
+    },
+    "filter": {
+        "term": {
+            "passwordProtected": false 
+        }
+    },
+    "from" : 0, "size" : 10,
+    "sort" : [
+        {"uploaded" : "desc"}
+   ]
+}
--- es-feed.php
+++ es-feed.php
@@ -27,23 +27,28 @@ left join file_hash_multimedia fhm using (hashid)
 left join file_upload_data fud on (f.id=fud.file_id)
 where id > 108774660 and status='ok' and public='public' and banned=0 
 and virusFound<2 and displayStatus not in ('illegal','maybe_illegal')
-limit 10000;
+limit 100000;
 
 							 ")) {
 	echo "Number of files: " . mysqli_num_rows($result);
     /* fetch associative array */
 
+    $success = 0; $fail = 0;
+
     while ($row = $result->fetch_assoc()) {
-        $command = "curl -XPUT \"http://localhost:9200/files/public/{$row['id']}\" -d'";
+        $command = "curl -s -XPUT \"http://localhost:9200/files/public/{$row['id']}\" -d'";
     	unset($row['id']);
-        $command .= json_encode($row) . "'";
-        echo "\n\n" . $command;
-        $res = shell_exec($command);
-        $response = json_decode($res);
+        $command .= json_encode($row, JSON_HEX_APOS | JSON_UNESCAPED_UNICODE) . "'";
+        echo "\n" . $command;
+        $rawResponse = shell_exec($command);
+        $response = json_decode($rawResponse);
         if ($response->created === true) {
-            echo "\nsuccess";
+            echo "\nSuccess";
+            $success++;
         } else {
-            var_dump('failed', $response);
+            //var_dump('failed', $response);
+            echo "\nFail with: " . $rawResponse;
+            $fail++;
         }
     }
 
@@ -57,7 +62,7 @@ limit 10000;
 
 $mysqli->close();
 
-echo "\n\nFinished\n";
+echo "\n\nFinished with OKs: {$success}   and   KOs: {$fail}\n";
 
 
 exit();
@@ -94,13 +99,13 @@ PUT files
             "type": "string"
         },
         "sizeInKB": {
-            "type": "integer"
+            "type": "long"
         },
         "realm": {
             "type": "string"
         },
         "rating": {
-            "type": "integer"
+            "type": "short"
         },
         "quality":   {
             "type": "string"
@@ -122,7 +127,58 @@ PUT files
   }
 }
 
+# klasicky relevatni search, dnesni strict
+POST /files/_search
+{
+    "query": {
+        "query_string": {
+            "query": "petr rar",
+            "default_operator": "and",
+            "default_field": "keywords"
+        }
+    },
+    "from" : 0, "size" : 10
+}
 
+# nebo asi lepsi, novejsi
+POST /files/_search
+{
+    "query": {
+        "query_string": {
+            "query": "petr rar",
+            "default_field": "keywords",
+            "use_dis_max" : true
+        }
+    },
+    "from" : 0, "size" : 10
+}
+
+# boostujeme obsah z neceskych geiop
+POST /files/_search
+{
+    "query": {
+        "bool": {
+            "must": {
+                "match": {
+                    "keywords": { 
+                        "query":    "petr rar",
+                        "operator": "and"
+                    }
+                }
+            },
+            "should": [ 
+                { "match": { "geoipcountry": "EG" }}
+            ]
+        }
+    }
+}
+
+
+
+# skrze fuzziness muzeme mozna vyresit i preklepy zamerne preklepy v nazvech!!!
+
+
+# sorting vypina relevanci !!
 POST /files/_search
 {
     "query": {
@@ -142,20 +198,63 @@ POST /files/_search
    ]
 }
 
-
+# agreguje podle hashid a sortuje podle cetnosti, size=0 nevraci zadny resultset krome agregaci
 POST /files/_search
 {
-    "match": {
-        "keywords": "petr rar",
-        "operator" : "and"
-    },
-    "from" : 0, "size" : 10
+  "size": 0,
+  "aggs": {
+    "group_by_hashid": {
+      "terms": {
+        "field": "hashid"
+      }
+    }
+  }
+}
+
+# pocet dokumentu s unikatnim hashid
+POST /files/_search
+{
+  "size": 0,
+  "aggs": {
+    "hashid_count": {
+      "cardinality": {
+        "field": "hashid"
+      }
+    }
+  }
+}
+
+# nejvetsi ci nejmensi hashid
+POST /files/_search
+{
+  "size": 0,
+  "aggs": {
+    "hashid_min_max": {
+      "max": {
+        "field": "hashid"
+      }
+    }
+  }
 }
 
 
 // boost limit strict search
 
 // dodat i file_category!!! nejak
+
+select count(*) from file f
+left join file_hashflags fh using (hashid)
+left join file_hash_multimedia fhm using (hashid)
+left join file_upload_data fud on (f.id=fud.file_id)
+where status='ok' and public='public' and banned=0 and size!=0
+and virusFound<2 and displayStatus not in ('illegal','maybe_illegal');
+
++----------+
+| count(*) |
++----------+
+| 18832293 |
++----------+
+1 row in set (11 min 30.66 sec)
 
 select f.id, name, keywords_name as keywords, f.hashid, f.upload_date as uploaded, uploader_geoipcountry as geoipcountry, 
 contentType, fh.size as sizeInKB, if(displayStatus in ('safe', 'maybe_safe'),'UT','PF') as realm, rating_status as rating, 
@@ -166,6 +265,6 @@ from file f
 left join file_hashflags fh using (hashid)
 left join file_hash_multimedia fhm using (hashid)
 left join file_upload_data fud on (f.id=fud.file_id)
-where id > 108774660 and status='ok' and public='public' and banned=0 
+where id > 108774660 and status='ok' and public='public' and banned=0 and size!=0 
 and virusFound<2 and displayStatus not in ('illegal','maybe_illegal')
 limit 10;
