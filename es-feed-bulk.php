5e44fae17c5f64bf0c32d6e80511445b1cc8405f
Marek Raida <marek.raida@lmc.eu>
2016-06-09 16:02:05 +0200

    Some config&desc finetunning to bulk indexer experiment...

--- es-feed-bulk.php
+++ es-feed-bulk.php
@@ -1,50 +1,74 @@
 <?php
-ini_set('memory_limit', '2048M');
-require ('/Users/xrevelon/git/ulozto-web/Nodus/Security/IntEncrypt.php');
-require ('/Users/xrevelon/cnf.php');
+// ElasticSearch data feeder for UT file data, experimental, by MR
 
-echo "\n\n\n";
+// script is quite resource demanding, so reserve enough RAM for it
+ini_set('memory_limit', '2048M');
+// read necessary DB credentials
+require ('/Users/xrevelon/cnf.php');
 
 $mysqli = new mysqli($cnf['h'], $cnf['u'], $cnf['p'], $cnf['d']);
 
 if ($mysqli->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
+    printf("Connection failed: %s\n", $mysqli->connect_error);
     exit();
 }
 
-$lastFileId = 110000000;
-$success = 0; $fail = 0; 
-$totalDocs = 0;
-$startAt = time();
-$fedingOffset = 100000;
-
+// query to get highest file_id from the db
 $result = $mysqli->query("select max(id) mx from file");
-$lastFileId = (int) $result->fetch_assoc()['mx'];
 
+
+
+
+
+// we are indexing from most recent (most important) to eldest documents
+$lastFileId = $lastFileId = (int) $result->fetch_assoc()['mx'];
+// to be able to use sorting without too much overhead, we should narrow the window
+$fedingOffset = 100000;
+// limit of docs of one indexing batch call
+$limit = 20000;
+
+// ES index name, ES type name and ES indexer node bulk api address
+$indexName = "files4";
+$typeName = "public";
+$esIndexerBulkUrl = "http://localhost:9200/_bulk"; // "http://es1.farm.int.nds:9200/_bulk"
+
+
+
+
+
+
+// simple counters for succeded, refused sends and overall number of documents send to the search
+$success = 0; $fail = 0; $totalDocs = 0;
+
+// just timestamp to be able to measure indexing duration
+$startAt = time();
+
+// inerate until there is still something to do
 while ($lastFileId) {
 
     echo "\nExecuting SQL for last file.id={$lastFileId}, elapsed time in seconds: " . (int)(time() - $startAt);
+
     if ($result = $mysqli->query("
-
-    select f.id, name, keywords_name as keywords, f.hashid, f.upload_date as uploaded, uploader_geoipcountry as geoipcountry, 
-    contentType, fh.size as sizeInKB, if(displayStatus in ('safe', 'maybe_safe'),'UT','PF') as realm, rating_status as rating, 
-    if(fhm.width>=720 and fhm.height>=720,'HQ','LQ') as quality, length as lengthInSec, 
-    if(streamable,1,0) as streamable, if(thumbVideo='',false,true) as liveFreeStreaming, 
-    if(archiveProtected>0 and password is not null,true,false) as archiveProtected, if(password,true,false) as passwordProtected 
-    from file f
-    left join file_hashflags fh using (hashid)
-    left join file_hash_multimedia fhm using (hashid)
-    left join file_upload_data fud on (f.id=fud.file_id)
-    where id between ". ($lastFileId-$fedingOffset) ." and {$lastFileId} and status='ok' and public='public' and banned=0 and flags2 not like '%searchable%'
-    and virusFound<2 and displayStatus not in ('illegal','maybe_illegal') 
-    order by id desc limit 15000;
-
+        select f.id, name, keywords_name as keywords, f.hashid, f.upload_date as uploaded, uploader_geoipcountry as geoipcountry, 
+        contentType, fh.size as sizeInKB, if(displayStatus in ('safe', 'maybe_safe'),'UT','PF') as realm, rating_status as rating, 
+        if(fhm.width>=720 and fhm.height>=720,'HQ','LQ') as quality, length as lengthInSec, 
+        if(streamable,1,0) as streamable, if(thumbVideo='',false,true) as liveFreeStreaming, 
+        if(archiveProtected>0 and password is not null,true,false) as archiveProtected, if(password,true,false) as passwordProtected 
+        from file f
+        left join file_hashflags fh using (hashid)
+        left join file_hash_multimedia fhm using (hashid)
+        left join file_upload_data fud on (f.id=fud.file_id)
+        where id between ". ($lastFileId-$fedingOffset) ." and {$lastFileId} and status='ok' and public='public' and banned=0 
+        and flags2 not like '%searchable%' and virusFound<2 and displayStatus not in ('illegal','maybe_illegal') 
+        order by id desc limit {$limit};
     							 ")) {
+
         $resultCount = mysqli_num_rows($result);
     	echo "\nNumber of files get: {$resultCount}, so far send OK {$totalDocs} documents...\n";
 
+        // evaluation current iteration, whether and how to continue next, based on actual data
         if (!$resultCount && ($lastFileI <= $fedingOffset)) {
-            echo ("\nNo more data to fetch, ending....");
+            echo ("\nNo more data to fetch, ending...");
             $lastFileId = 0;
             break;
         } elseif (!$resultCount) {
@@ -52,21 +76,24 @@ while ($lastFileId) {
             continue;
         }
 
+        // prepare command/payload of documents to be indexed; hint: we are assuming that named index&type already exists!
         $command = "";
         while ($row = $result->fetch_assoc()) {
-        	$command .= '{ "index" : { "_index" : "files4", "_type" : "public", "_id" : "' . $row['id'] . '" } }' . "\n";
+        	$command .= '{ "index" : { "_index" : "' . $indexName . '", "_type" : "' . $typeName . '", "_id" : "' . $row['id'] . '" } }' . "\n";
             $lastFileId = $row['id'];
             unset($row['id']);
             $command .= json_encode($row, JSON_HEX_APOS | JSON_UNESCAPED_UNICODE) . "\n";
         }
 
+        // create temp file with payload to send and if we succeed, delete it, otherwise left for later re-send/analysis
         $temp = tempnam('.', 'feed');
         file_put_contents($temp, $command);
-        $rawResponse = shell_exec('curl -s -XPOST "http://localhost:9200/_bulk" --data-binary "@'.$temp.'"');
-        //$rawResponse = shell_exec('curl -XPOST "http://es1.farm.int.nds:9200/_bulk" --data-binary "@'.$temp.'"');
+
+        // execute the send command itself and evaluate its result
+        $rawResponse = shell_exec('curl -s -XPOST "' . $esIndexerBulkUrl . '" --data-binary "@'.$temp.'"');
         $response = json_decode($rawResponse);
         if ($response->errors === false) {
-            echo "\nSuccessfil send";
+            echo "\nSuccessful send";
             $success++;
             $totalDocs += $resultCount;
             // tidy up the mess in current directory
@@ -74,26 +101,28 @@ while ($lastFileId) {
         } else {
             echo "\nFailed send on {$temp} file with: " . $rawResponse;
             $fail++;
-            // do not unlink file to keep it for potential analysis
+            // do not unlink file to keep it for potential re-send or analysis
         }
 
+        // free current SQL statement
         $result->close();
-
+        // and at least try to feebly free some memory
         gc_collect_cycles();
     }
 }
 
 
-
-
+// and we're almost done
 $mysqli->close();
 
 echo "\n\nFinished with OKs: {$success} and KOs: {$fail} of batches... in ".(time()-$startAt)." seconds \n";
 
-
-exit();
+exit(); // done
 
 ?>
+
+Unimportant stuff and bluff follows...
+
 
 DELETE /files4
 
