90b62d9f6c5bec7d9468f14863aa4a792f2570ed
Marek Raida <marek.raida@lmc.eu>
2016-06-10 15:42:54 +0200

    Some more queries + tuned up search index a bit more...

--- es-feed-bulk.php
+++ es-feed-bulk.php
@@ -2,7 +2,7 @@
 // ElasticSearch data feeder for UT file data, experimental, by MR
 
 // script is quite resource demanding, so reserve enough RAM for it
-ini_set('memory_limit', '2048M');
+ini_set('memory_limit', '4096M');
 // read necessary DB credentials
 require ('/Users/xrevelon/cnf.php');
 
@@ -21,16 +21,18 @@ $result = $mysqli->query("select max(id) mx from file");
 
 
 // we are indexing from most recent (most important) to eldest documents
-$lastFileId = $lastFileId = (int) $result->fetch_assoc()['mx'];
+$lastFileId = (int) $result->fetch_assoc()['mx'];
+//$lastFileId = 1158; // override for cases of unexpected crash... elder than 14.2M are much much slower to feed...
 // to be able to use sorting without too much overhead, we should narrow the window
-$fedingOffset = 100000;
+$feedingOffset = 90000;
 // limit of docs of one indexing batch call
 $limit = 20000;
 
 // ES index name, ES type name and ES indexer node bulk api address
-$indexName = "files4";
+$indexName = "files5";
 $typeName = "public";
-$esIndexerBulkUrl = "http://localhost:9200/_bulk"; // "http://es1.farm.int.nds:9200/_bulk"
+//$esIndexerBulkUrl = "http://localhost:9200/_bulk";
+$esIndexerBulkUrl = "http://es1.farm.int.nds:9200/_bulk";
 
 
 
@@ -44,35 +46,37 @@ $success = 0; $fail = 0; $totalDocs = 0;
 $startAt = time();
 
 // inerate until there is still something to do
-while ($lastFileId) {
+while ($lastFileId > 0) {
 
     echo "\nExecuting SQL for last file.id={$lastFileId}, elapsed time in seconds: " . (int)(time() - $startAt);
 
     if ($result = $mysqli->query("
         select f.id, name, keywords_name as keywords, f.hashid, f.upload_date as uploaded, uploader_geoipcountry as geoipcountry, 
         contentType, fh.size as sizeInKB, if(displayStatus in ('safe', 'maybe_safe'),'UT','PF') as realm, rating_status as rating, 
-        if(fhm.width>=720 and fhm.height>=720,'HQ','LQ') as quality, length as lengthInSec, 
-        if(streamable,1,0) as streamable, if(thumbVideo='',false,true) as liveFreeStreaming, 
+        if(fhm.width>=720 and fhm.height>=720,'HQ','LQ') as quality, length as lengthInSec, if(pornHumanCheck=2,1,0) as gayPorn, 
+        if(streamable,1,0) as streamable, if(thumbVideo='' and flags2 like '%videoshare%',false,true) as liveFreeStreaming, 
         if(archiveProtected>0 and password is not null,true,false) as archiveProtected, if(password,true,false) as passwordProtected 
         from file f
         left join file_hashflags fh using (hashid)
         left join file_hash_multimedia fhm using (hashid)
         left join file_upload_data fud on (f.id=fud.file_id)
-        where id between ". ($lastFileId-$fedingOffset) ." and {$lastFileId} and status='ok' and public='public' and banned=0 
+        where id between ". ($lastFileId-$feedingOffset) ." and {$lastFileId} and status='ok' and public='public' and banned=0 
         and flags2 not like '%searchable%' and virusFound<2 and displayStatus not in ('illegal','maybe_illegal') 
         order by id desc limit {$limit};
     							 ")) {
 
         $resultCount = mysqli_num_rows($result);
-    	echo "\nNumber of files get: {$resultCount}, so far send OK {$totalDocs} documents...\n";
+    	echo "\nNumber of files get:  {$resultCount},  so far send OK {$totalDocs} documents, with memory usage in MB: " 
+            . round(memory_get_usage(true)/1048576,2) . "\n";
 
         // evaluation current iteration, whether and how to continue next, based on actual data
-        if (!$resultCount && ($lastFileI <= $fedingOffset)) {
+        if (!$resultCount && ($lastFileId <= $feedingOffset)) {
             echo ("\nNo more data to fetch, ending...");
             $lastFileId = 0;
             break;
         } elseif (!$resultCount) {
-            $lastFileId -= $fedingOffset;
+            $lastFileId -= $feedingOffset;
+            echo ("\nNot enough data in set, shifting lastFileId to " . $lastFileId);
             continue;
         }
 
@@ -81,8 +85,8 @@ while ($lastFileId) {
         while ($row = $result->fetch_assoc()) {
         	$command .= '{ "index" : { "_index" : "' . $indexName . '", "_type" : "' . $typeName . '", "_id" : "' . $row['id'] . '" } }' . "\n";
             $lastFileId = $row['id'];
-            unset($row['id']);
-            $command .= json_encode($row, JSON_HEX_APOS | JSON_UNESCAPED_UNICODE) . "\n";
+            $command .= json_encode(cleanRecord($row), JSON_HEX_APOS | JSON_UNESCAPED_UNICODE) . "\n";
+            //$command .= json_encode(cleanRecord($row), JSON_HEX_APOS | JSON_UNESCAPED_UNICODE) . "\n";
         }
 
         // create temp file with payload to send and if we succeed, delete it, otherwise left for later re-send/analysis
@@ -104,10 +108,18 @@ while ($lastFileId) {
             // do not unlink file to keep it for potential re-send or analysis
         }
 
-        // free current SQL statement
+        // free current SQL statement and at least try to feebly free some memory...
         $result->close();
-        // and at least try to feebly free some memory
+        unset($command);
+        unset($result);
+        unset($resultCount);
+        unset($row);
+        unset($temp);
+        unset($rawResponse);
+
         gc_collect_cycles();
+    } else {
+        echo "DB Query failed...";
     }
 }
 
@@ -119,16 +131,32 @@ echo "\n\nFinished with OKs: {$success} and KOs: {$fail} of batches... in ".(tim
 
 exit(); // done
 
+// helper function cleaning out all unnecessary items and reduce index size
+function cleanRecord($row) {
+    unset($row['id']);
+    if ($row['geoipcountry'] === null) unset($row['geoipcountry']);
+    if ($row['contentType'] === '') unset($row['contentType']);
+    if (!in_array($row['contentType'], array('video', 'audio', 'image'))) unset($row['quality']);
+    if (!in_array($row['contentType'], array('video', 'audio'))) unset($row['lengthInSec']);
+    // predelat vsechno na tagy????
+    if (!$row['streamable']) unset($row['streamable']);
+    if (!$row['liveFreeStreaming']) unset($row['liveFreeStreaming']);
+    if (!$row['archiveProtected']) unset($row['archiveProtected']);
+    if (!$row['passwordProtected']) unset($row['passwordProtected']);
+    if (!$row['gayPorn']) unset($row['gayPorn']);
+    return $row;
+}
+
 ?>
 
 Unimportant stuff and bluff follows...
 
 
-DELETE /files4
+DELETE /files5
 
-GET /files4/_stats
+GET /files5/_stats
 
-PUT /files4
+PUT /files5
 {
   "mappings": {
     "public": {
@@ -175,6 +203,9 @@ PUT /files4
             "type": "boolean"
         },
         "passwordProtected": {
+            "type": "boolean"
+        },
+        "gayPorn": {
             "type": "boolean"
         }
       }
@@ -271,3 +302,68 @@ POST /files3/_search
     }
 }
 
+last id indexed so far...
+39455592
+
+# ziskani nejvyssiho ID dokumentu
+GET /files4/_search
+{
+  "fields": [
+    "_id"
+  ],
+  "query": {
+    "match_all": {}
+  },
+  "sort": {
+    "_id": "desc"
+  },
+  "size": 1
+}
+
+# pokus o negativni boosting, ne prilis uspesny, ty do + jdou mnohem lepe!!
+POST /files/_search
+{
+    "query": {
+        "bool": {
+            "must": {
+                "match": {  
+                    "keywords": {
+                        "query":    "petr rar",
+                        "operator": "and"
+                    }
+                }
+            },
+            "should": [
+                { "match": {
+                    "geoipcountry": {
+                        "query": "EG",
+                        "boost": 0.000000001 
+                    }
+                }}
+            ]
+        }
+    }
+}
+
+# todo: range or grater than
+
+# nemame last-modified, takze zrychlit mazani muzeme mozna tim, ze vsechny "DEL" si budme psat do vedlejsi tabulky a promazavat podle ni a nebo ten sloupec pridat
+# multi_match by se mohl hodit po zavedeni tagu
+# pridat gaye a vykopat nepotrebne fieldy???
+
+# range request pro videa mezi 5m a 1h
+POST /files4/_search
+{
+    "query" : {
+        "constant_score" : {
+            "filter" : {
+                "range" : {
+                    "lengthInSec" : {
+                        "gte" : 300,
+                        "lte"  : 3600
+                    }
+                }
+            }
+        }
+    }
+}
